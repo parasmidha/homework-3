c32b3834bdcc88f1056e4f2348c7482e77feaf84
parasmidha <paras.midha@kellogg.northwestern.edu>
2022-05-19 03:28:33 +0000

    Defined places and posts controllers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,22 @@
 class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+    end
+
+    def show
+        @place = Place.find_by({ "id" => params["id"] })
+        @posts = Post.where({"place_id" => params["id"]})
+      end
 end
+
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+    def new
+        @post = Post.new
+        @post["place_id"] = params["place_id"]
+        @place = Place.find_by({ "id" => params["place_id"]})
+    end
+
+    def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/places"
+    end
 end
